3d23772f19090c44ffba1ff87beab98637d0f492
EvgenyKarataev <karataev.evgeny@gmail.com>
2015-08-27 14:55:57 -0400

    first version of vagrantfile with mysql, neo4j, and carte

--- VagrantBootstrap.sh
+++ VagrantBootstrap.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+echo "installing apache2"
+
 apt-get update
 apt-get install -y apache2
 
-sudo service apache2 restart
+#sudo service apache2 restart
 
 #if ! [ -L /var/www ]; then
 #  rm -rf /var/www
@@ -20,8 +22,51 @@ echo mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD | de
 
 apt-get install -y mysql-server
 
-service mysql start
+#service mysql start
 
 echo "running sql script"
 
 mysql -uroot -p$MYSQL_PASSWORD < /vagrant/ColfusionDb.sql
+
+echo "installing java"
+
+apt-get install -y openjdk-7-jdk
+
+export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"
+
+echo "copying and extracting neo4j"
+
+cp /vagrant/neo4j-community-1.9.2.tar.gz /opt
+
+cd /opt
+tar -xzf neo4j-community-1.9.2.tar.gz
+
+chown -R root:root neo4j-community-1.9.2
+
+echo "starting neo4j"
+
+cd neo4j-community-1.9.2
+mkdir data
+
+sed -i 's/#org.neo4j.server.webserver.address=0.0.0.0/org.neo4j.server.webserver.address=0.0.0.0/g' /opt/neo4j-community-1.9.2/conf/neo4j-server.properties
+
+./bin/neo4j start
+
+echo "creating neo4j indexes"
+
+curl -X POST -H "Content-Type: application/json" -d '{"name":"sources","config":{"provider":"lucene","type":"exact"}}' http://localhost:7474/db/data/index/node
+curl -X POST -H "Content-Type: application/json" -d '{"name":"rels","config":{"provider":"lucene","type":"exact"}}' http://localhost:7474/db/data/index/relationship
+
+echo "installing git"
+
+apt-get install -y git
+
+echo "downloading pentaho"
+
+cd /opt
+git clone https://github.com/ColFusion/PentahoKettle.git
+cd PentahoKettle/kettle-data-integration
+nohup ./carte.sh 0.0.0.0 8081 &
+ 
+echo "done"
+
