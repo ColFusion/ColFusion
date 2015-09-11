16f49cd31861727f7946578eb5e0c1e508d970a6
Evgeny Karataev <Karataev.Evgeny@gmail.com>
2015-09-11 10:07:30 -0400

    Fix wrot dir to run db script refs #11

--- up.sh
+++ up.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+COLFUSION_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )
+
 echo $(pwd)
 
 vagrant up
@@ -14,10 +16,12 @@ vagrant up
 
 echo "Running maven to install all dependencies (skipping running tests)"
 
-cd ColfusionServer
+cd $COLFUSION_DIR/ColfusionServer
 
 mvn install -DskipTests
 
 echo "Run flyway migrations to setup database tables"
+
+cd $COLFUSION_DIR/Colfusion
 
 sh ./db_migrate.sh
