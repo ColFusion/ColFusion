85ce62e0371097f3f7d13ba958b6750fce91d81c
Dan <das178@pitt.edu>
2015-09-27 13:42:52 -0400

    simpler

--- pullall.sh
+++ pullall.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
 # This script pulls the latest code for the checked-out branch of colfusion repos
+# (conflicts may prevent pulling. Running pullall.sh is useful to see if all repos updated)
 
 COLFUSION_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )
-
-cd $COLFUSION_DIR
 
 REPOS=(ColFusion ColfusionOpenRefine ColfusionServer ColfusionWeb PentahoKettle)
 
 for repo in "${REPOS[@]}"
 do
-    cd "$repo"
+    cd "$COLFUSION_DIR/$repo"
     git pull
-    cd ..
 done
