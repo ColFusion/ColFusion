e703b4e159a6280438be7080b36e0014e9f911d2
Dan <das178@pitt.edu>
2015-09-30 16:07:20 -0400

    forgot sigil

--- up.sh
+++ up.sh
@@ -27,14 +27,14 @@ until [ -z "$1" ]; do
 done
 
 if [ "${PROD}" == true ]; then
-      if [ -z "CF_CPUS" ]; then
+      if [ -z "${CF_CPUS}" ]; then
             CF_CPUS="$(nproc)"
             if [ $CF_CPUS -gt 1 ]; then
                 CF_CPUS="$(($CF_CPUS-1))"
             fi
             export CF_CPUS
       fi
-      if [ -z "CF_MEM" ]; then
+      if [ -z "${CF_MEM}" ]; then
             CF_MEM="$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 }')"
             CF_MEM="$(($CF_MEM/1024))" # convert to megabytes
             CF_MEM="$((($CF_MEM*7)/10))" # use 70%
