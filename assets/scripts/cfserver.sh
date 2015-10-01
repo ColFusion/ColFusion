5b07f71cd38383a8a0d544bacca4ea72a5be5d1c
Dan <das178@pitt.edu>
2015-10-01 12:37:35 -0400

    create config.properties within the VM

--- assets/scripts/cfserver.sh
+++ assets/scripts/cfserver.sh
@@ -7,6 +7,8 @@ set -o errexit
 
 # Don't remove files after building. Needed for building openrefine.
 
+CF_DIR="/opt/Colfusion"
+
 WEBAPPS="/opt/apache-tomcat-8.0.26/webapps"
 
 mkdir -p /opt/build
@@ -15,6 +17,14 @@ rm -rf ColfusionServer
 
 cp -r /opt/Colfusion/ColfusionServer .
 cd ColfusionServer/
+
+PROPS="ColFusionServerUtils/src/main/resources/config.properties"
+: > "${PROPS}" # creates zero-length file (or wipes existing file to zero-length)
+echo "colfusion.properties.source = custom in main" >> "${PROPS}"
+[ -e "${CF_DIR}/ColFusion" ] && echo "colfusion.static_files.root_location = ${CF_DIR}/ColFusion/assets/www" >> "${PROPS}"
+[ -e "${CF_DIR}/ColfusionOpenRefine" ] && echo "colfusion.openrefine.folder = ${CF_DIR}/ColfusionOpenRefine/workspace" >> "${PROPS}"
+[ -e "${CF_DIR}/ColfusionOpenRefine" ] && echo "colfusion.openrefine.csv_file_dir = ${CF_DIR}/ColfusionOpenRefine/workspace" >> "${PROPS}"
+
 mvn clean
 mvn install -DskipTests
 TARGET="$(ls -t ColFusionServerWAR/target/*.war | head -1)"
