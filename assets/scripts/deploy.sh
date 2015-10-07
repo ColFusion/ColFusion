47ebebbd0402eb6d5c27fab1d91d80e97f907458
Dan <das178@pitt.edu>
2015-10-07 13:50:31 -0400

    missing quote

--- assets/scripts/deploy.sh
+++ assets/scripts/deploy.sh
@@ -8,7 +8,7 @@ set -o errexit
 CF_DIR="/opt/Colfusion"
 
 "${CF_DIR}/ColFusion/assets/scripts/cfserver.sh"
-"${CF_DIR}/ColFusion/assets/scripts/cfserver-init.sh
+"${CF_DIR}/ColFusion/assets/scripts/cfserver-init.sh"
 
 "${CF_DIR}/ColFusion/assets/scripts/carte.sh"
 
