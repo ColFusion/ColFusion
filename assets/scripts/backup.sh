4be502052a773cc10617d895b2d31699bbf5157b
Dan <das178@pitt.edu>
2015-10-15 15:22:29 -0400

    neo4j backup

--- assets/scripts/backup.sh
+++ assets/scripts/backup.sh
@@ -18,6 +18,16 @@ mkdir -p "${WORKINGDIR}/${TIMESTAMP}"
 
 cd "${WORKINGDIR}/${TIMESTAMP}"
 
+# Shut down colfusion while backing up. This is necessary for neo4j.
+# Backing up while running neo4j is possible for neo4j enterprise and
+# also possibly v2 of neo4j community.
+# Additionally, shutting down will ensure backed up data is consistent
+# (that is, e.g., mysql data reflects same point of time as neo4j data)
+/etc/init.d/apache2 stop
+# does stop wait for completion of outstanding requests?
+# in case not, let's wait a few seconds
+sleep 10
+
 ##### Start transfers
 
 ## Colfusion
@@ -34,9 +44,13 @@ mysqldump --all-databases -uroot -p"${MYSQL_PASSWORD}" > mysql.sql
 
 ## Neo4J
 
-# ???
+/etc/rc6.d/K99_stop_neo4j
+cp -r /opt/neo4j-community/data/graph.db Neo4jGraphDb
+/etc/init.d/start_neo4j
 
 ##### End transfers
+
+/etc/init.d/apache2 start
 
 cd "${WORKINGDIR}"
 # TODO: is it faster to make tarball in VM, and then transfer to shared folder?
