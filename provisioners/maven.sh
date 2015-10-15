145e2b617c7ed8d764e0963e2c4a4ac2470299a0
Dan <das178@pitt.edu>
2015-10-15 14:46:20 -0400

    generic maven path too

--- provisioners/maven.sh
+++ provisioners/maven.sh
@@ -10,9 +10,15 @@ cp /opt/Colfusion/ColFusion/assets/software/apache-maven-3.3.3-bin.tar.gz /opt
 cd /opt
 tar -xzf apache-maven-3.3.3-bin.tar.gz
 
+if [ -h apache-maven ]; then
+      rm apache-maven
+fi
+
+ln -s apache-maven-3.3.3 apache-maven
+
 # TODO: modify PATH as opposed to this
-ln -s /opt/apache-maven-3.3.3/bin/mvn /usr/bin
-ln -s /opt/apache-maven-3.3.3/bin/mvnDebug /usr/bin
-ln -s /opt/apache-maven-3.3.3/bin/mvnyjp /usr/bin
+ln -s /opt/apache-maven/bin/mvn /usr/bin
+ln -s /opt/apache-maven/bin/mvnDebug /usr/bin
+ln -s /opt/apache-maven/bin/mvnyjp /usr/bin
 
 echo "Done with maven"
